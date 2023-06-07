c8d5076ae2ec15eadfabc9e6da5a5c1db6bcba15
Nikhil Sethi <nikhil@Nikhils-MacBook-Pro.local>
2023-06-06 23:03:59 -0700

    collab poem

---
+++ script.js
@@ -0,0 +1,20 @@
+var button1 = document.getElementById("button1");
+var div1 = document.getElementById("1");
+
+button1.addEventListener("click", function() {
+      div1.classList.toggle("show");
+    });
+
+var button2 = document.getElementById("button2");
+var div2 = document.getElementById("2");
+
+button2.addEventListener("click", function() {
+      div2.classList.toggle("show");
+    });
+
+var button3 = document.getElementById("button3");
+var div3 = document.getElementById("3");
+
+button3.addEventListener("click", function() {
+      div3.classList.toggle("show");
+    });
